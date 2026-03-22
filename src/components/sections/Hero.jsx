47456bc6fe4e77dc4a2f1b51f8b57a008f08ff91
Takaharu Suzuki <taka.webtree@gmail.com>
2026-03-22 01:28:27 -0600

    removed font-weight setting from @font-face

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <>
       {/* <Header/> */}
-      <section className="w-screen min-h-dvh header-gradient-bg relative">
-        <h1 className="text-h1">Takaharu Suzuki</h1>
+      <section className="w-screen min-h-dvh header-gradient-bg">
+        <h1 className="text-sky-950 text-h1">Takaharu Suzuki</h1>
       </section>
     </>
   )
