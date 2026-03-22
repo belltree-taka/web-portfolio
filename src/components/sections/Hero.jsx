ca4516dabbedaab44042d39a9e70160aa6889030
Takaharu Suzuki <taka.webtree@gmail.com>
2026-03-22 01:14:11 -0600

    Changed layer level from components to utilities

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,10 +6,7 @@ const Hero = () => {
     <>
       {/* <Header/> */}
       <section className="w-screen min-h-dvh header-gradient-bg relative">
-        <h1 className="text-6xl md:text-7xl text-center font-heading text-sky-950 px-6 absolute left-1/2 top-1/2 -translate-1/2">
-            <p className="text-animation mb-4">TAKAHARU SUZUKI</p>
-            <p className="md:text-3xl">PORTFOLIO</p>
-        </h1>
+        <h1 className="text-h1">Takaharu Suzuki</h1>
       </section>
     </>
   )
