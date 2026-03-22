3fcc883e9711863cf203712f4ed5a7019c1493c9
Takaharu Suzuki <taka.webtree@gmail.com>
2026-03-21 22:15:49 -0600

    Restore App.css and created fonts folder for Albert Sans woff2

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -5,10 +5,13 @@ const Hero = () => {
   return (
     <>
       <Header/>
-      <div>Hero</div>
-      
+      <section className="w-screen min-h-svh header-gradient-bg relative">
+        <h1 className="text-6xl md:text-7xl text-center font-heading text-sky-950 px-6 absolute left-1/2 top-1/2 -translate-1/2">
+            <p className="text-animation mb-4">TAKAHARU SUZUKI</p>
+            <p className="md:text-3xl">PORTFOLIO</p>
+        </h1>
+      </section>
     </>
-    
   )
 }
 
