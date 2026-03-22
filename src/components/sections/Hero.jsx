b27d94faf95845e2ea8983ef460ecf80fbfdabf2
Takaharu Suzuki <taka.webtree@gmail.com>
2026-03-22 00:51:44 -0600

    Created font and spacing setting

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,8 +4,8 @@ import Header from '../layout/Header'
 const Hero = () => {
   return (
     <>
-      <Header/>
-      <section className="w-screen min-h-svh header-gradient-bg relative">
+      {/* <Header/> */}
+      <section className="w-screen min-h-dvh header-gradient-bg relative">
         <h1 className="text-6xl md:text-7xl text-center font-heading text-sky-950 px-6 absolute left-1/2 top-1/2 -translate-1/2">
             <p className="text-animation mb-4">TAKAHARU SUZUKI</p>
             <p className="md:text-3xl">PORTFOLIO</p>
